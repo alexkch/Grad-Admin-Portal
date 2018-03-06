ad1eb913b6678c6cfa859756c2bcbaf8ed101e2d
alexkch <alexc0219@gmail.com>
2018-03-06 13:25:47 -0500

    set up test db and prod db configs

--- app/index.js
+++ app/index.js
@@ -1,13 +1,13 @@
 //set NODE_ENV=development or production
 const config = require('config');
 //set DEBUg=enabled
-const Debugger = require('debug')('enabled');
+const Debugger = require('debug')('DEBUG');
 const morgan = require('morgan');
 const express = require('express');
 const app = express();
 
 console.log('Application Name: ' + config.get('app_name'));
-Debugger('Debug Enabled');
+Debugger('Enabled');
 if (config.get('env') === 'dev') {
   console.log('Starting Morgan');
   app.use(morgan('tiny'));
@@ -18,7 +18,7 @@ require('./startup/db')();
 require('./startup/config')();
 require('./startup/obj-validation')();
 
-Debugger('Key: ' + config.get('jwtPrivateKey'));
+Debugger('JWT Key: ' + config.get('jwtPrivateKey'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}...`));
