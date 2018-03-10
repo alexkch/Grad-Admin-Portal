4cf665a8cae20c69862232618a34216559e7b66a
Chris <christonechen@gmail.com>
2018-03-09 21:25:40 -0500

    Create create user component

---
+++ react_fe/src/components/User/CreateUser.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+
+class CreateUser extends Component{
+	constructor(props){
+		super(props);
+		this.state = {email: '', password: '', name: '', usertype: 'faculty', isAdmin: false}
+		
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleChangeValue = this.handleChangeValue.bind(this);
+
+	}
+	handleSubmit(){
+		// Create tickets
+	}
+
+	handleChangeValue(event){
+		const name = event.target.name;
+    	let value = event.target.value;
+		if(event.target.type === 'checkbox'){
+			value = event.target.checked;
+		}
+		this.setState({ [name]: value });
+	}
+	render(){
+		let createUser = (
+			<div className='CreateUser'>
+				<form onSubmit={ this.handleSubmit }>
+					<label>
+						Email:
+	        			<input name='email' type="email" value={this.state.email} onChange={this.handleChangeValue} />
+					</label>
+					<label>
+						Password:
+	        			<input name="password" type="password" value={this.state.password} onChange={this.handleChangeValue} />
+					</label>
+					<label>
+						Name:
+	        			<input name="name" type="text" value={this.state.name} onChange={this.handleChangeValue} />
+					</label>
+					<label>
+						<input name='usertype' type="radio" value="faculty"
+							checked={this.state.usertype === "faculty"}
+							onChange={this.handleChangeValue} />
+
+						Faculty
+	        		</label>
+					<label>
+						<input name='usertype' type="radio" value="budget_office"
+							checked={this.state.usertype === "budget_office"}
+							onChange={this.handleChangeValue} />
+						Budget Office
+	        		</label>
+	        		<label>
+						<input name='usertype' type="radio" value="grad_office"
+							checked={this.state.usertype === "grad_office"}
+							onChange={this.handleChangeValue} />
+						Grad Office
+	        		</label>
+	        		<label>
+						<input name='isAdmin' type="checkbox" checked={this.state.isAdmin}
+							onChange={this.handleChangeValue} />
+						Admin
+	        		</label>				
+        		</form>
+
+			</div>
+		);
+		return createUser;
+	}
+
+}
+export default CreateUser;
