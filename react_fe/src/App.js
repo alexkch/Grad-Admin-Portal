fba1abcfee20a7551d57b954608dbeb0fb768d94
alexkch <alexc0219@gmail.com>
2018-03-10 13:31:33 -0500

    added classnames dependency

--- react_fe/src/App.js
+++ react_fe/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
 import styles from './App.css';
-
 import Dashboard from './containers/Dashboard/Dashboard';
 
 class App extends Component {
   render() {
     return (
       <div className={styles.App}>
+        <h1 className={classNames(styles.italic, styles.red)}>My APP</h1>
         <Dashboard />
       </div>
     );
