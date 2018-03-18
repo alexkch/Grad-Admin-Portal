774dec5cd30edd734241991c0668eb645ad9c466
alexkch <alexc0219@gmail.com>
2018-03-18 02:00:04 -0400

    small typo; removed mobile nav for now

--- app/react_fe/src/components/ui/navigation/NavItems.js
+++ app/react_fe/src/components/ui/navigation/NavItems.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './NavItems.css';
 
-const navItems = ( props ) => (
+const navItems = () => (
     <ul className={styles.NavItems}>
       <li className={styles.NavItem}>
         <a href="/">Link 1</a>
