275da3ff8379772578dfe0c5272c7150b2d07d71
WangYiran <qq644876085@gmail.com>
2018-03-19 09:40:02 -0400

    First version of Front end.                         TODO: small componments.

--- app/react_fe/src/App.js
+++ app/react_fe/src/App.js
@@ -1,16 +1,29 @@
-import React, { Component } from 'react';
-import classNames from 'classnames';
-import styles from './App.css';
+import React, {Component} from 'react';
+import style from './css/bootstrap.min.css';
 import Dashboard from './containers/Dashboard/Dashboard';
 
 class App extends Component {
   render() {
     return (
-      <div className={styles.App}>
-        <h1 className={classNames(styles.italic, styles.red)}>My APP</h1>
+        <div>
+            <nav
+                className={style.navbar + " " + style["navbar-expand-lg"] + " " + style["navbar-dark"] + " " + style["bg-primary"]}>
+                <a className={style["navbar-brand"]} href="#">Ticket System</a>
+                <div className={style.collapse + " " + style["navbar-collapse"]} id="navbarColor01">
+                    <ul className={style["navbar-nav"] + " " + style["mr-auto"]}>
+                        <li className={style["nav-item"] + " " + style.active}>
+                            <a className={style["nav-link"]} href="#">Home <span
+                                className={style["sr-only"]}>(current)</span></a>
+                        </li>
+
+                    </ul>
+                </div>
+
+            </nav>
+
         <Dashboard />
       </div>
-    );
+    )
   }
 }
 
