52fc04f077931f2414893a320754c70e07ed052f
alexkch <alexc0219@gmail.com>
2018-03-20 18:01:33 -0400

    added nav links to nav bar

--- app/react_fe/src/components/navigation/Nav.js
+++ app/react_fe/src/components/navigation/Nav.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 
 import sty from '../../css/bootstrap.min.css';
 import User from '../../containers/user/User'
+import NavItems from './NavItems';
 
 class Navigator extends Component {
 
@@ -11,7 +12,7 @@ class Navigator extends Component {
                 className={sty.navbar + " " + sty["navbar-expand-lg"] + " " + sty["navbar-dark"] + " " + sty["bg-dark"]}>
                 <a className={sty["navbar-brand"]} href="#">Ticket System</a>
                 <User/>
-
+                <NavItems />
             </nav>
 
 
