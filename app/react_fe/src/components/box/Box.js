ded38b9d8cd0945c920a0a9efbb4faf1ee9ca588
WangYiran <qq644876085@gmail.com>
2018-03-22 20:40:40 -0400

    Added color options for box class

--- app/react_fe/src/components/box/Box.js
+++ app/react_fe/src/components/box/Box.js
@@ -2,13 +2,41 @@ import React, {Component} from 'react';
 
 import sty from '../../css/bootstrap.min.css';
 
-const box = (props) => (
-  <div className={sty.card + " " + sty["text-white"] + " " + sty["bg-dark"] + " " + sty["mb-3"]}>
-      <div className={sty["card-header"]}>{props.title}</div>
-      <div className={sty["card-body"]}>
-        {props.body}
-      </div>
-  </div>
-);
+
+
+
+const box = (props) => {
+    let boxColor = sty["text-white"] + " " + sty["bg-dark"];
+    if (props.color){
+        switch (props.color){
+            case('primary'):
+                boxColor = sty["text-white"] + " " + sty["bg-dark"];
+                break;
+            case('secondary'):
+                boxColor = + sty["bg-secondary"];
+                break;
+            case('info'):
+                boxColor = sty["text-white"] + " " + sty["bg-info"];
+                break;
+            case('success'):
+                boxColor = sty["text-white"] + " " + sty["bg-success"];
+                break;
+            case('danger'):
+                boxColor = sty["text-white"] + " " + sty["bg-danger"];
+                break;
+            default:
+                boxColor = sty["text-white"] + " " + sty["bg-dark"];
+
+        }
+    }
+
+
+    return (<div className={sty.card + " " + boxColor +" " + sty["mb-3"]}>
+        <div className={sty["card-header"]}>{props.title}</div>
+        <div className={sty["card-body"]}>
+            {props.body}
+        </div>
+    </div>)
+};
 
 export default box;
