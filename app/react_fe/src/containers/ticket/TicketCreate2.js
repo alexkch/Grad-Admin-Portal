b96548e6c5eaf95bf3cecb64c366f6b565b2c32b
Chrion01 <christonechen@gmail.com>
2018-03-28 12:51:12 -0400

    2nd version of ticket create based on new issue.

---
+++ app/react_fe/src/containers/ticket/TicketCreate2.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react';
+import Form from '../../components/form/Form';
+import checkValidity from '../../utils/validateForm';
+import Box from '../../components/box/Box';
+import Button from '../../components/button/Button';
+
+class TicketCreate2 extends Component {
+
+    state = {
+        form: {
+            professor: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Faculty'
+                },
+                value: '',
+                validation: {
+                    required: false
+                },
+                valid: false,
+                touched: false
+            },
+            number: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'number',
+					min: '0'
+                    placeholder: 'Number of tickets to create'
+                },
+                value: '0',
+                validation: {
+                    required: true
+                },
+                valid: true,
+                touched: false
+            },
+            status: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        {value: 'granted', displayValue: 'Granted'},
+                        {value: 'redeemed', displayValue: 'Redeemed'},
+                    ]
+                },
+                value: '',
+                validation: {},
+                valid: true
+            }
+        },
+        formIsValid: false,
+    }
+
+    postHandler = ( event ) => {
+        event.preventDefault();
+        const formData = {};
+        for (let input in this.state.form) {
+            formData[input] = this.state.form[input].value;
+        }
+    }
+
+    inputChangedHandler = (event, inputIdentifier) => {
+        const updatedform = {
+            ...this.state.form
+        };
+        const updatedFormElement = {
+            ...updatedform[inputIdentifier]
+        };
+        updatedFormElement.value = event.target.value;
+        updatedFormElement.valid = checkValidity(updatedFormElement.value, updatedFormElement.validation);
+        updatedFormElement.touched = true;
+        updatedform[inputIdentifier] = updatedFormElement;
+
+        let formIsValid = true;
+        for (let inputIdentifier in updatedform) {
+            formIsValid = updatedform[inputIdentifier].valid && formIsValid;
+        }
+        this.setState({form: updatedform, formIsValid: formIsValid});
+    }
+
+    render () {
+        const formElementsArray = [];
+        for (let key in this.state.form) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.form[key]
+            });
+        }
+        let form = (
+            <form onSubmit={this.postHandler}>
+                {formElementsArray.map(formElement => (
+                    <Form
+                        key={formElement.id}
+                        elementType={formElement.config.elementType}
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value}
+                        invalid={!formElement.config.valid}
+                        shouldValidate={formElement.config.validation}
+                        touched={formElement.config.touched}
+                        changed={(event) => this.inputChangedHandler(event, formElement.id)} />
+                ))}
+                <Button disabled={!this.state.formIsValid} type={'disabled-stretch'}>Submit</Button>
+            </form>
+        );
+        return (
+            <Box color="secondary" header={"Create new Tickets"}>{form}</Box>
+        );
+    }
+}
+
+export default TicketCreate2;
