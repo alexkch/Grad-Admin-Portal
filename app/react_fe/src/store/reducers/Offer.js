c73632d0fdf03f418371d0d34f9b34b82b822219
alexkch <alexc0219@gmail.com>
2018-03-30 02:08:06 -0400

    offer reducer added

--- app/react_fe/src/store/reducers/Offer.js
+++ app/react_fe/src/store/reducers/Offer.js
@@ -3,7 +3,6 @@ import update from '../../utils/update';
 
 const initialState = {
     offers: [],
-    rerender: false,
     error: false,
     errorMsg: ''
 };
@@ -12,23 +11,46 @@ const reducer = ( state = initialState, action ) => {
     switch ( action.type ) {
         case actionTypes.SET_OFFERS:
             return update(state, {
-              error : false,
+              error : action.error,
               offers : action.offers});
 
         case actionTypes.SET_OFFERS_FAIL:
             return update(state, {
-              error : true,
-              offers : action.errorMsg});
+              error : action.error,
+              errorMsg : action.errorMsg});
 
         case actionTypes.POST_OFFER:
             return state;
 
         case actionTypes.POST_OFFER_SUCCESS:
-            return update(state, {error : false} );
+            return update(state, {error : action.error} );
 
         case actionTypes.POST_OFFER_FAIL:
-            return update(state, {error : true} );
+            return update(state, {
+              error : action.error,
+              errorMsg: action.errorMsg});
 
+        case actionTypes.DELETE_OFFER:
+            return state;
+
+        case actionTypes.DELETE_OFFER_SUCCESS:
+            return update(state, {error : action.error} );
+
+        case actionTypes.DELETE_OFFER_FAIL:
+            return update(state, {
+              error : action.error,
+              errorMsg: action.errorMsg});
+
+        case actionTypes.UPDATE_OFFER:
+            return state;
+
+        case actionTypes.UPDATE_OFFER_SUCCESS:
+            return update(state, {error : action.error} );
+
+        case actionTypes.UPDATE_OFFER_FAIL:
+            return update(state, {
+              error : action.error,
+              errorMsg: action.errorMsg});
         default:
             return state;
     }
