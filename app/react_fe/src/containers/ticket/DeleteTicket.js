a3a0f6c68c5c4d4735b1415e418c41ae03ab363a
Chrion01 <christonechen@gmail.com>
2018-03-31 12:46:33 -0400

    delete offer and ticket added

---
+++ app/react_fe/src/containers/ticket/DeleteTicket.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as Actions from '../../store/actions';
+
+class DeleteTicket extends Component {
+    componentDidMount () {
+      this.props.deleteTicket(this.props.token, this.props.match.params.id);
+    }
+
+    render () {
+        return <Redirect to="/tickets"/>;
+    }
+}
+
+const mapStateToProps = state => {
+  return {
+      token: state.user.token,
+      error: state.ticket.error,
+      errorMsg: state.ticket.errorMsg
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    deleteTicket: (token, id) => dispatch(Actions.deleteTicket(token, id))
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(DeleteTicket);
